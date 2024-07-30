22bde3a898e91dc810efe3ccf4cd4b87618e0d90
uuujini <youjin010402@naver.com>
2024-07-30 12:48:29 +0900

    [CHORE]: import문 수정

--- financial_statements/financial_server.py
+++ financial_statements/financial_server.py
@@ -7,9 +7,6 @@ import os
 from datetime import datetime, timedelta
 import re
 import json
-import sys
-
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 app = FastAPI()
 
